d60ad214e406239fe8149c491302a159dec7263d
eseshinpu <eseshinpu@example.com>
2020-08-21 11:20:55 +0900

    [fix] quick fix.

--- 2_openmp/main.py
+++ 2_openmp/main.py
@@ -3,21 +3,6 @@ import time
 from multithread import countNumber, countNumberUsingOpenmp
 
 
-def stop_watch(func):
-    """Time measurement decorator
-
-    reference：https://qiita.com/hisatoshi/items/7354c76a4412dffc4fd7
-    """
-    @wraps(func)
-    def wrapper(*args, **kargs):
-        start = time.time()
-        result = func(*args, **kargs)
-        elapsed_time = time.time() - start
-        print(f"{func.__name__} took {elapsed_time}秒かかりました")
-        return result
-    return wrapper
-
-
 def count_num_py(num):
     start = time.time()
     for i in range(num):
